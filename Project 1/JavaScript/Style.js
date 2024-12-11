fd0284c2d3b5f0f21c9092bf10c91f0d5790b380
Ben Redmond <73973708+BenRedmondgithub@users.noreply.github.com>
2024-12-11 13:05:01 +0000

    just deming stuff

--- Project 1/JavaScript/Style.js
+++ Project 1/JavaScript/Style.js
@@ -3,3 +3,20 @@ console.log('Hello World')
 console.log(100)
 
 console.log(20, 'Hello', true)
+
+const x = 3000;
+
+console.log(x);
+
+
+//Ways to Declare a Variable 
+// var, let, const
+
+let firstName = 'Bill'
+let lastName = 'Mcdonold'
+
+let fullName = 'Bill Mcdonold'
+
+let age = 65;
+
+console.log( fullName, age, lastName, firstName)
